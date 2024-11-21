bbc987cf39d8063bd92efd808f107adf5af13c97
Johnson Osita <80931376+Jermaine-jay@users.noreply.github.com>
2024-11-21 11:45:46 +0100

    Chore: Removed sensitive data

--- SocialAuth.NET/Controllers/OpenIdController.cs
+++ SocialAuth.NET/Controllers/OpenIdController.cs
@@ -12,12 +12,12 @@ namespace SocialAuth.NET.Controllers
         public OpenIdController() { }
 
         [HttpPost("microsoft-login")]
-        public async Task<IActionResult> MicrosoftAuth(string credential)
+        public async Task<IActionResult> MicrosoftAuth(string token)
         {
-            OpenIdConfiguration res = new OpenIdConfiguration(credential);
-            //var result = res.googleIdConfiguration.ValidateGoogleToken("286600713169-1frq8hfoq4hsl1qm760f9v17u1bvh9fe.apps.googleusercontent.com");
+            OpenIdConfiguration res = new OpenIdConfiguration(token);
+            //var result = res.googleIdConfiguration.ValidateGoogleToken("b");
             //var result = res.appleIdConfiguration.ValidateAppleToken("a");
-            var result = res.microsoftIdConfiguration.ValidateMicrosoftToken("a62fb4cf-b78f-4fcd-9b0c-05944747f732", "9188040d-6c67-4c5b-b112-36a304b66dad");
+            var result = res.microsoftIdConfiguration.ValidateMicrosoftToken("c", "d");
             return Ok(result);
         }
     }
