612167f32ffc6a2c0c2fb9be6de6ef1c5449c7cf
Johnson Osita <80931376+Jermaine-jay@users.noreply.github.com>
2025-01-26 22:16:18 +0100

    Enhancement: Added Different Controllers

--- SocialAuth.NET/Controllers/OpenIdController.cs
+++ SocialAuth.NET/Controllers/OpenIdController.cs
@@ -9,16 +9,39 @@ namespace SocialAuth.NET.Controllers
     {
         public OpenIdController() { }
 
-        [HttpPost("openId-auth")]
-        public async Task<IActionResult> OpenIdAuth(string token)
+        [HttpPost("google-openId-auth")]
+        public async Task<IActionResult> GoogleOpenIdAuth(string token)
         {
-            OpenIdConfiguration res = new OpenIdConfiguration(token);
-            string microsoftUser = res.microsoftIdConfiguration.ValidateMicrosoftToken("a62fb4cf-b78f-4fcd-9b0c-05944747f732", "9188040d-6c67-4c5b-b112-36a304b66dad");
+            OpenIdConfiguration openIdConfiguration = new OpenIdConfiguration(token);
+            string googleUser = openIdConfiguration.googleIdConfiguration.ValidateGoogleToken("your-app-audience");
+            
+            return Ok(googleUser);
+        }
 
-            //string googleUser = res.googleIdConfiguration.ValidateGoogleToken("286600713169-1frq8hfoq4hsl1qm760f9v17u1bvh9fe.apps.googleusercontent.com");
-            //string appleUser = res.appleIdConfiguration.ValidateAppleToken("your-app-audience");
-            //string fecebookUser = res.facebookIdConfiguration.ValidateFacebookToken("your-app-id", "your-app-secet");
+        [HttpPost("microsoft-openId-auth")]
+        public async Task<IActionResult> MicrosoftOpenIdAuth(string token)
+        {
+            OpenIdConfiguration openIdConfiguration = new OpenIdConfiguration(token);
+            string microsoftUser = openIdConfiguration.microsoftIdConfiguration.ValidateMicrosoftToken("your-app-audience", "your-app-tenantid");
+
             return Ok(microsoftUser);
+        }
+
+        [HttpPost("apple-openId-auth")]
+        public async Task<IActionResult> AppleOpenIdAuth(string token)
+        {
+            OpenIdConfiguration openIdConfiguration = new OpenIdConfiguration(token);
+            string appleUser = openIdConfiguration.appleIdConfiguration.ValidateAppleToken("your-app-audience");
+
+            return Ok(appleUser);
+        }
+
+        [HttpPost("facebook-openId-auth")]
+        public async Task<IActionResult> FacebookOpenIdAuth(string token)
+        {
+            OpenIdConfiguration openIdConfiguration = new OpenIdConfiguration(token);
+            Payload fecebookUser = openIdConfiguration.facebookIdConfiguration.ValidateFacebookToken("your-app-id", "your-app-secet");
+            return Ok(fecebookUser);
         }
     }
 }
